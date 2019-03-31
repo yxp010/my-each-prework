5ec4a532b8e1a8fbc692135294e537fe781a919c
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-31 04:43:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each（words) # put argument(s) here
   # code here
+  counter = 0
+  while counter < words.count
+    yield(words[counter])
+    counter += 1
+  end
+
+  words
 end
